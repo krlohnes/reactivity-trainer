afcb71504e538e3e5689f54ca9ec6642844f77c2
Keith Lohnes <krlohnes@users.noreply.github.com>
2023-05-20 20:52:35 -0400

    Add model select and animation

--- main.js
+++ main.js
@@ -1,52 +1,99 @@
 import * as THREE from './nm/three/build/three.module.js';
 import { GLTFLoader } from './nm/three/examples/jsm/loaders/GLTFLoader.js';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-// Create a light source
-var light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 0, 10);
-scene.add(light);
+var scene, camera, renderer, model, mixer, clock, distanceSlider, modelDropdown;
 
-// Load the dog model
-var loader = new GLTFLoader();
-loader.load(
-  'dog_model.glb',
-  function (gltf) {
-    // Retrieve the dog mesh from the loaded model
-    var dog = gltf.scene;
-    scene.add(dog);
-  },
-  undefined,
-  function (error) {
-    console.error('Error loading GLB model:', error);
+// Initialize the scene, camera, and renderer
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  var light = new THREE.PointLight(0xffffff, 1, 100);
+  light.position.set(0, 0, 10);
+  scene.add(light);
+
+  distanceSlider = document.getElementById('distanceSlider');
+  modelDropdown = document.getElementById('modelDropdown');
+
+  // Load the initial model
+  loadModel(modelDropdown.value);
+
+  // Add event listeners
+  distanceSlider.addEventListener('input', render);
+  modelDropdown.addEventListener('change', onModelChange);
+
+  render();
+}
+
+// Load the selected model
+function loadModel(modelPath) {
+  if (model) {
+    scene.remove(model);
   }
-);
 
-// Adjust the camera position based on the slider value
+  var loader = new GLTFLoader.GLTFLoader();
+  loader.load(
+    modelPath,
+    function (gltf) {
+      model = gltf.scene;
+
+      // Create the animation mixer
+      mixer = new THREE.AnimationMixer(model);
+
+      // Play the first animation clip found
+      var clips = gltf.animations;
+      if (clips && clips.length > 0) {
+        var clip = clips[0];
+        var action = mixer.clipAction(clip);
+        action.play();
+      }
+
+      scene.add(model);
+    },
+    undefined,
+    function (error) {
+      console.error('Error loading model:', error);
+    }
+  );
+}
+
+// Handle model selection change
+function onModelChange() {
+  var modelPath = modelDropdown.value;
+  loadModel(modelPath);
+  render();
+}
+
+// Update the camera position based on the slider value
 function updateCameraPosition() {
-  var distance = parseFloat(document.getElementById('distanceSlider').value);
+  var distance = parseFloat(distanceSlider.value);
   camera.position.z = distance;
 }
 
 // Render the scene
 function render() {
   updateCameraPosition();
+
+  // Update the animation mixer
+  if (mixer) {
+    var delta = clock.getDelta();
+    mixer.update(delta);
+  }
+
   renderer.render(scene, camera);
 }
 
-// Initialize the slider event listener and start rendering
+// Initialize the app
 window.addEventListener('load', function () {
-  var slider = document.getElementById('distanceSlider');
-  slider.addEventListener('input', render);
-  render();
+  clock = new THREE.Clock();
+  init();
 });
